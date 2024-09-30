9027908e3abc4337330ac21236e26a331c5b87ec
Alex <125302863+alexky0@users.noreply.github.com>
2024-09-30 08:55:44 -0400

    started series

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("hello world")
+import pandas as pd
+import numpy as np
 
+data = pd.Series([0.25, 0.5, 0.75, 'hi'])
+print(data)
+grade = pd.Series(["freshmen", "sophomore", "junior", "senior"], index=[9, 10, 11, 12])
+print(grade[9])
 
-if __name__ == "__main__":
-    main()
+cookies_dict = {
+    "double chocolate": 10,
+    "chocolate chip": 8,
+    "oatmeal raisin": 6,
+    "snickerdoodle": 7,
+    "dirt": 1
+}
+
+cookies = pd.Series(cookies_dict)
+
+print("dirt rating: ", cookies["dirt"])
