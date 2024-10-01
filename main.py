202fd7a4468c5cd9162cfdfeeed76234a5e109b0
Alex <125302863+alexky0@users.noreply.github.com>
2024-10-01 15:11:30 -0400

    dataframe and loc

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import pandas as pd
 import numpy as np
 
 data = pd.Series([0.25, 0.5, 0.75, 'hi'])
-print(data)
 grade = pd.Series(["freshmen", "sophomore", "junior", "senior"], index=[9, 10, 11, 12])
-print(grade[9])
 
 cookies_dict = {
     "double chocolate": 10,
@@ -16,4 +14,22 @@ cookies_dict = {
 
 cookies = pd.Series(cookies_dict)
 
-print("dirt rating: ", cookies["dirt"])
+cookies_df = pd.DataFrame(cookies, columns=["rating"])
+
+cookies_df["allergens"] = [True, True, True, True, False]
+
+cookies_df["sweetness"] = {
+    "double chocolate": 10,
+    "chocolate chip":  9,
+    "oatmeal raisin": 7,
+    "snickerdoodle": 8,
+    "dirt": -1,
+    "dinosaur": 11
+}
+
+
+data = pd.Series(['a', 'c', 'e', 'g'], index=[1, 3, 5, 7])
+
+print(data)
+
+print(data.iloc[2:5])
