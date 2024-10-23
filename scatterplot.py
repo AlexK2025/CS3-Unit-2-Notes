175ed65ecba4faa879ce589bbf9cb96816b2bdc1
Alex <125302863+alexky0@users.noreply.github.com>
2024-10-23 10:40:18 -0400

    scaterplog

--- scatterplot.py
+++ scatterplot.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import random
+
+from sklearn.datasets import load_iris
+
 plt.style.use("seaborn-v0_8-bright")
 
-
-x = np.linspace(0, 10, 100)
-y = np.cos(x)
-
-plt.scatter(x, y, marker="o", color="gray")
+iris = load_iris()
+features = iris.data.T
+plt.scatter(features[0], features[1], s=100*features[3], c=iris.target, cmap="viridis")
+plt.xlabel(iris.feature_names[0])
+plt.ylabel(iris.feature_names[1])
 
 plt.savefig("scatterplot.png")
