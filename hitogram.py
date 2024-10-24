a9c0e0bb9e7430ed6d74423561b2118e755ed869
Alex <125302863+alexky0@users.noreply.github.com>
2024-10-24 14:25:37 -0400

    histogram  black hole

--- hitogram.py
+++ hitogram.py
@@ -3,9 +3,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 plt.style.use("seaborn-v0_8-bright")
 
-rng = np.random.default_rng(100)
-data = rng.normal(size=1000)
+rng = np.random.default_rng(1000)
 
-plt.hist(data)
+mean = [0, 0]
+cov = [[1,1], [1,2]]
+x, y = rng.multivariate_normal(mean, cov, 100000000000000).T
+plt.hexbin(x, y, gridsize=1000000)
+cb  = plt.colorbar(label="count in bin")
 
 plt.savefig("histogram.png")
